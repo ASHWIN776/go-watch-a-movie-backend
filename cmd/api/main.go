fca7b5fdfef185ccea5690caa08ef095a3f63dad
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-25 15:41:57 +0530

    Initial Files Setup

---
+++ cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8000
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// set application config
+	var app application
+
+	// Read command line flags
+
+	// Connect to db
+
+	app.Domain = "example.com"
+
+	// Start a web server
+	log.Printf("Listening on %d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
